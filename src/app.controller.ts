7274c357bf02899c55ea40d6893c2cf42bc3a981
Jose Aguirre <josepabloaguirre02@gmail.com>
2024-06-18 12:25:23 -0600

    Update: app.controller 1.0 examples

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,16 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello World!';
+  }
+
+  @Get('new')
+  newEndopoint() {
+    return 'Yo soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'con /sas/';
   }
 }
